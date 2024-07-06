bb060c1582c5b4104d190f45c123540bd167a396
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-07-05 23:20:31 -0500

    Add: Constructor and tick component

--- Source/CryptRaider/TriggerZoneComponent.cpp
+++ Source/CryptRaider/TriggerZoneComponent.cpp
@@ -3,8 +3,26 @@
 
 #include "TriggerZoneComponent.h"
 
+UTriggerZoneComponent::UTriggerZoneComponent()
+{
+
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+    UE_LOG(LogTemp, Log, TEXT("Trigger Zone constructor"));
+}
+
+
+
 void UTriggerZoneComponent::BeginPlay()
 {
     Super::BeginPlay();
     UE_LOG(LogTemp, Display, TEXT("Box Component Works"));
+}
+
+
+void UTriggerZoneComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
--- Source/CryptRaider/TriggerZoneComponent.h
+++ Source/CryptRaider/TriggerZoneComponent.h
@@ -14,12 +14,17 @@ class CRYPTRAIDER_API UTriggerZoneComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 
-
+public:
+	UTriggerZoneComponent();
 
 protected: 
 	virtual void BeginPlay() override;
 
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
 
 	
 };
